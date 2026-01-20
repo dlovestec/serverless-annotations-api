3bde7fe3754e1974e9f15893e14f91aab2f08bea
Dharmesh Chaudhary <chaudharydharmesh32@gmail.com>
2026-01-21 02:30:02 +1100

    Removed cors
    - Uninstalled cors, removed from app.js and serverless.yml

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const annotationsRouter = require('./routes/annotations');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 app.use('/annotations', annotationsRouter);
